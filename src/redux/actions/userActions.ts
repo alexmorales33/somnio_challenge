7f2320bd81503c9689a509c0bbea54abcffa0372
alexmorales33 <alexmoraleess4@gmail.com>
2024-06-07 15:40:49 -0300

    new .env

--- src/redux/actions/userActions.ts
+++ src/redux/actions/userActions.ts
@@ -2,7 +2,7 @@ import { User } from '@/types/userTypes'
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
   
-const API_URL = process.env.NEXT_PUBLIC_SERVER_BASE_URL
+const API_URL = process.env.API_URL
 
 export const getAllUsers = createAsyncThunk<User[]>('users/getAll', async () => {
     const response = await axios.get<User[]>(`${API_URL}/users`)
